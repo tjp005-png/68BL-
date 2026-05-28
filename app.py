536f08a07f21a4056310805eb909558959aa6e31
Pereira <pereira.taylor@cleanharbors.com>
2026-05-28 09:10:42 -0700

    Patch flask-socketio startup crash by allowing unsafe werkzeug

--- app.py
+++ app.py
@@ -219,6 +219,6 @@ def portal_hub():
 
 if __name__ == '__main__':
     start_backup_scheduler(app)
-    socketio.run(app, host='0.0.0.0', port=5002, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5002, debug=True, allow_unsafe_werkzeug=True)
 
 
