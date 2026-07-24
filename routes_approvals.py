1dc8cafb7f7570e27448a62b0bdb9677bdd5912d
Pereira <pereira.taylor@cleanharbors.com>
2026-07-24 14:27:07 -0700

    style(approvals): set row height of LDR Required (row 18) to 20pt to match section formatting

--- routes_approvals.py
+++ routes_approvals.py
@@ -1505,13 +1505,7 @@ def export_wvi_excel(profile_number):
         if reg[0] in ["DOT Description:", "LDR Required:"]:
             ws.merge_cells(f'B{row}:D{row}')
             ws[f'B{row}'].alignment = Alignment(wrap_text=True, vertical='center')
-            text_len = len(str(reg[1] or ''))
-            if text_len > 70:
-                ws.row_dimensions[row].height = 36
-            elif text_len > 40:
-                ws.row_dimensions[row].height = 24
-            else:
-                ws.row_dimensions[row].height = 20
+            ws.row_dimensions[row].height = 20
         else:
             ws[f'C{row}'].value = reg[2]
             ws[f'D{row}'].value = reg[3]
